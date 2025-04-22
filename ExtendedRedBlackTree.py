42d102c89e9678bfa7b38dca5bafbb42c69f801e
blw046586 <104181815+blw046586@users.noreply.github.com>
2025-04-21 19:54:18 -0500

    Create ExtendedRedBlackTree.py

---
+++ ExtendedRedBlackTree.py
@@ -0,0 +1,125 @@
+from ExtendedRBTNode import ExtendedRBTNode
+
+class ExtendedRedBlackTree:
+    def __init__(self):
+        self.root = None
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left:
+            y.left.parent = x
+        y.parent = x.parent
+        if not x.parent:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+        x.update_subtree_key_count()
+        y.update_subtree_key_count()
+
+    def right_rotate(self, y):
+        x = y.left
+        y.left = x.right
+        if x.right:
+            x.right.parent = y
+        x.parent = y.parent
+        if not y.parent:
+            self.root = x
+        elif y == y.parent.right:
+            y.parent.right = x
+        else:
+            y.parent.left = x
+        x.right = y
+        y.parent = x
+
+        y.update_subtree_key_count()
+        x.update_subtree_key_count()
+
+    def insert(self, key):
+        node = ExtendedRBTNode(key)
+        y = None
+        x = self.root
+
+        while x:
+            y = x
+            if key < x.key:
+                x = x.left
+            else:
+                x = x.right
+
+        node.parent = y
+        if not y:
+            self.root = node
+        elif key < y.key:
+            y.left = node
+        else:
+            y.right = node
+
+        self.fix_insert(node)
+
+    def fix_insert(self, z):
+        while z.parent and z.parent.color == 'red':
+            if z.parent == z.parent.parent.left:
+                y = z.parent.parent.right
+                if y and y.color == 'red':
+                    z.parent.color = y.color = 'black'
+                    z.parent.parent.color = 'red'
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.right:
+                        z = z.parent
+                        self.left_rotate(z)
+                    z.parent.color = 'black'
+                    z.parent.parent.color = 'red'
+                    self.right_rotate(z.parent.parent)
+            else:
+                y = z.parent.parent.left
+                if y and y.color == 'red':
+                    z.parent.color = y.color = 'black'
+                    z.parent.parent.color = 'red'
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.left:
+                        z = z.parent
+                        self.right_rotate(z)
+                    z.parent.color = 'black'
+                    z.parent.parent.color = 'red'
+                    self.left_rotate(z.parent.parent)
+        self.root.color = 'black'
+        self._update_counts_upward(z)
+
+    def _update_counts_upward(self, node):
+        while node:
+            node.update_subtree_key_count()
+            node = node.parent
+
+    def get_nth_key(self, n):
+        def helper(node, n):
+            if not node:
+                return None
+            left_count = node.left.subtree_key_count if node.left else 0
+            if n < left_count:
+                return helper(node.left, n)
+            elif n == left_count:
+                return node.key
+            else:
+                return helper(node.right, n - left_count - 1)
+
+        return helper(self.root, n)
+
+    def in_order_traversal(self, node=None, result=None):
+        if result is None:
+            result = []
+        if node is None:
+            node = self.root
+        if node.left:
+            self.in_order_traversal(node.left, result)
+        result.append(node.key)
+        if node.right:
+            self.in_order_traversal(node.right, result)
+        return result
